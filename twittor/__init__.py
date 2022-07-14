0fcec6dbbe39b07beeac2533d0f653fda708b801
Andrew <a129924@gmail.com>
2022-07-15 00:18:05 +0800

    add

--- twittor/__init__.py
+++ twittor/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
-from twittor.route import index, login
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+from twittor.route import index, login  # NOQA: E402
 
 def create_app():
     app = Flask(__name__)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////:twittor.db" # 要////
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_COMMIT_TEARDOWN'] = True
+    
     app.config['SECRET_KEY'] = "123456"
     app.add_url_rule('/index',"index",index) # 等同於@app.route("/")
     app.add_url_rule('/login',"login",login,methods = ["GET", "POST"]) 
---
+++ twittor/models.py
@@ -0,0 +1,11 @@
+from twittor import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(64))
+    
+    def __repr__(self):
+        return f"id={self.id},username={self.username},email={self.username},password={self.password}"
+
