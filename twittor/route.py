85b49e70f6e41e11a1f322960910eeac998e1b02
Andrew <a129924@gmail.com>
2022-07-17 21:24:00 +0800

    add

--- twittor/route.py
+++ twittor/route.py
@@ -31,23 +31,23 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for("index"))
     else:
-        form = LoginForm()
-        if form.validate_on_submit():
-            username = User.query.filter_by(username = form.username.data).first()
+        login_form = LoginForm()
+        if login_form.validate_on_submit():
+            username = User.query.filter_by(username=login_form.username.data).first()
 
-            if username is None or username.check_password(form.password.data):
+            if username is None or username.check_password(login_form.password.data):
                 print("invalid username or password!")
                 # error = "invalid username or password!"
                 return redirect(url_for("login"))
             
-            login_user(username, remember=form.rememberMe.data)
+            login_user(username, remember=login_form.rememberMe.data)
             next_page = request.args.get("next")
             if next_page:
                 return redirect(next_page)
-            msg = f"username = {form.username.data}, password = {form.password.data}, rememberMe = {form.rememberMe.data}"
+            msg = f"username = {login_form.username.data}, password = {login_form.password.data}, rememberMe = {login_form.rememberMe.data}"
             print(msg)
         
-        return render_template("login.html", title="Login In", form=form)
+        return render_template("login.html", title="Login In", form=login_form)
 
 def logout():
     logout_user()
@@ -57,15 +57,19 @@ def register():
     print("re123")
     if current_user.is_authenticated:
         return redirect(url_for("index"))
-    form = RegisterForm()
+    register_form = RegisterForm()
 
-    print(f"re: {form.validate_on_submit()}")
-    if form.validate_on_submit():
+    print(f"re: {register_form.validate_on_submit()}")
+    
+    if register_form.validate_on_submit():
         print("re456")
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        # db.session.add(user)
-        # db.session.commit()
+        user = User(username=register_form.username.data,email=register_form.email.data)
+        user.set_password(register_form.password.data)
+        db.session.add(user)
+        db.session.commit()
         return redirect(url_for("login"))
     
-    return render_template("register.html",title="Registration", form=form)
+    msg = f"username = {register_form.username.data}, password = {register_form.password.data}, email = {register_form.email.data}"
+    print(msg)
+    
+    return render_template("register.html", title="Registration", form=register_form)
