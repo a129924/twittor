0f92d682d1228a508672da8788656fceb27eaf33
Andrew <a129924@gmail.com>
2022-07-17 20:29:22 +0800

    add

--- twittor/__init__.py
+++ twittor/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 
 from twittor.ext import db
-from twittor.route import index, login, logout
+from twittor.route import index, login, logout, register
 from twittor.config import Config
 from twittor.flask_login_manager import login_manager
 
@@ -12,7 +12,6 @@ from twittor.flask_login_manager import login_manager
 app = Flask(__name__)
 migrate = Migrate()
 login_manager.login_view = "login"
-
 
 def create_app():
     
@@ -26,5 +25,7 @@ def create_app():
     app.add_url_rule("/","index",index)
     app.add_url_rule('/login',"login",login,methods = ["GET", "POST"]) 
     app.add_url_rule("/logout", "logout", logout)
+    app.add_url_rule("/register", "register",register, methods=["GET", "POST"])
+    
     return app
 
--- twittor/route.py
+++ twittor/route.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, url_for, request
 from flask_login import login_user, current_user, logout_user, login_required
 
-from twittor.forms import LoginForm
+from twittor.forms import LoginForm ,RegisterForm
 from twittor.models import User, Tweet
+from twittor.ext import db
 
 @login_required
 def index():
@@ -51,3 +52,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("login"))
+
+def register():
+    print("re123")
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegisterForm()
+
+    print(f"re: {form.validate_on_submit()}")
+    if form.validate_on_submit():
+        print("re456")
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        # db.session.add(user)
+        # db.session.commit()
+        return redirect(url_for("login"))
+    
+    return render_template("register.html",title="Registration", form=form)
